675833dece61727fca36e68910d0aef989a17b20
Yason Prüfer <yason.pr@gmail.com>
2023-05-23 14:14:21 +0200

    watermark

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 /*
 |--------------------------------------------------------------------------
@@ -93,20 +94,41 @@ Route::post('/export/csv3', function(Request $request){
 
 
 // upload route 
-Route::post('/upload',function(Request $request) {
+Route::post('/upload', function (Request $request) {
+    // Validate file MIME type
+    $validatedData = $request->validate([
+        'file' => 'required|mimes:jpeg,png,gif',
+    ]);
 
-    // dd($request->file->getClientOriginalName());
-
+    // Handle file upload
     $fileName = $request->file->getClientOriginalName();
     $request->file->move(public_path('storage'), $fileName);
 
-    $request->file->store('public');
-    $request->file->storeAs('public', 'test.jpg');
+    // Create an Intervention Image instance from the uploaded file
+    try {
+        $image = Image::make(public_path('storage/' . $fileName));
+    } catch (\Exception $e) {
+        // Delete the uploaded file if it's not a valid image
+        unlink(public_path('storage/' . $fileName));
 
-    // return succes response
-    return response()->json(['success'=>'You have successfully upload file.']);
+        // Return error response
+        return response()->json(['error' => 'The uploaded file is not a valid image.']);
+    }
 
+    // Add a watermark to the image
+    $watermarkUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/128px-ChatGPT_logo.svg.png'; // Replace with the actual URL of your watermark image
+    $watermark = Image::make($watermarkUrl)->opacity(50);
+    $image->insert($watermark, 'center');
 
+    // Perform other desired image manipulations
+    $image->resize(300, 200);
+    // Add more manipulation methods as needed
+
+    // Save the modified image
+    $image->save(public_path('storage/' . $fileName));
+
+    // Return success response
+    return response()->json(['success' => 'You have successfully uploaded and edited the file.']);
 })->middleware('auth')->name('upload');
 
 // second way to protect routes with auth middleware using a group function
@@ -132,7 +154,7 @@ Route::post('/upload',function(Request $request) {
 
 // })-> middleware('auth')->name('upload');
 
-route::post('/upload', function(request $request) {
+route::post('/upload2', function(request $request) {
     // Validate file MIME type
     $validatedData = $request->validate([
         'file' => 'required|mimes:jpeg,png,gif',
@@ -145,4 +167,4 @@ route::post('/upload', function(request $request) {
     // Pass the image URL to the view
     $imageUrl = asset('storage/' . $fileName);
     return view('IMGupload', compact('imageUrl'));
-})->middleware('auth')->name('upload');
+})->middleware('auth')->name('upload2');
