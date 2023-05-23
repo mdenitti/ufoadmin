755ad64ac09cf8bbdb650e85c149deed488f239d
Alexandros Tsikakis <Alexandros.Tsikakis@ymail.com>
2023-05-23 14:03:09 +0200

    added resize, error handling and watermark

--- resources/views/IMGupload.blade.php
+++ resources/views/IMGupload.blade.php
@@ -1,3 +1,13 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+@if(isset($errorMessage))
+    <div class="container d-flex justify-content-center align-items-center">
+        <div class="alert alert-danger text-center" role="alert">
+            {{ $errorMessage }}
+        </div>
+    </div>
+@endif
+
 @if(isset($imageUrl))
     <img src="{{ $imageUrl }}" alt="Uploaded Image">
 @endif
